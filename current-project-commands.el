cb2a4c235b84fe81bc176b978d0b0ecaa344d954
Victor Deryagin <vderyagin@gmail.com>
2012-09-20 20:20:24 +0300

    `cpr-rspec' function

--- current-project-commands.el
+++ current-project-commands.el
@@ -84,4 +84,12 @@ Allows to specify tags generation command when called with C-u."
         (t
          (vc-dir root nil))))))
 
+;;;###autoload
+(defun cpr-rspec ()
+  "Uses `compilation-mode' to run 'rspec' command
+in context of the current project root directory"
+  (interactive)
+  (with-cpr-project
+    (compile "rspec")))
+
 (provide 'current-project-commands)
