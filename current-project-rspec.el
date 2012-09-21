41488431879b30585c8c01cb4e8a54ed95b99d03
Victor Deryagin <vderyagin@gmail.com>
2012-09-21 12:18:43 +0300

    `cpr-rspec' function: can run whole suite, one file or single spec

--- current-project-rspec.el
+++ current-project-rspec.el
@@ -1,9 +1,49 @@
+(require 'current-project)
+
+(define-compilation-mode rspec-mode "Rspec" "Mode for executing Rspec specs.")
+
+(setq rspec-scroll-output t)
+
+(add-hook 'rspec-start-hook (lambda () (goto-char (point-max))))
+(add-hook 'rspec-filter-hook
+          (defun rspec-colorize-buffer ()
+            (let ((inhibit-read-only t))
+              (ansi-color-apply-on-region (point-min) (point-max)))))
+
+
+(defun cpr-rspec (arg)
+  (interactive "p")
+  (cond
+    ((eq arg 1)
+     (cpr-rspec-execute-specs 'at-point))
+    ((eq arg 4)
+     (cpr-rspec-execute-specs 'file))
+    ((eq arg 16)
+     (cpr-rspec-execute-specs 'suite))))
+
 ;;;###autoload
-(defun cpr-rspec ()
-  "Uses `compilation-mode' to run 'rspec' command
-in context of the current project root directory"
+(defun cpr-rspec-execute-specs (scope)
+  "Executes rspec examples selected according to SCOPE.
+if SCOPE = 'at-point - example found at current point position.
+if SCOPE = 'file - examples found in currently visited file.
+if SCOPE = 'suite - whole rspec suite."
   (interactive)
   (with-cpr-project
-    (compile "rspec")))
+    (let ((file buffer-file-name)
+          (line-number (line-number-at-pos))
+          (command '("rspec")))
+
+      (when (eq scope 'at-point)
+        (add-to-list 'command "--line_number" 'append)
+        (add-to-list 'command (number-to-string line-number) 'append))
+
+      (unless (eq scope 'suite)
+        (add-to-list 'command (shell-quote-argument file) 'append))
+
+      (setq command (mapconcat 'identity command " "))
+
+      (cpr-save)
+
+      (compilation-start command 'rspec-mode))))
 
 (provide 'current-project-rspec)
