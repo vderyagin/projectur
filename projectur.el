fefc1b9a67bfac8389674fd0d25ebe43593dda46
Victor Deryagin <vderyagin@gmail.com>
2012-09-25 14:37:34 +0300

    projectur.el: added header

--- projectur.el
+++ projectur.el
@@ -1,4 +1,30 @@
 ;;; -*- lexical-binding: t -*-
+;;; projectur.el --- Support for projects in Emacs
+
+;; Copyright (C) 2012 Victor Deryagin
+
+;; Author: Victor Deryagin <vderyagin@gmail.com>
+;; Created: 3 Aug 2012
+;; Version: 0.0.3
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;;; Code:
 
 (eval-when-compile
   (require 'cl))
@@ -329,3 +355,5 @@ Supported version control systems: git, mercurial, subversion, cvs, darcs."
    (projectur-darcs-repo-p dir)))
 
 (provide 'projectur)
+
+;;; projectur.el ends here
